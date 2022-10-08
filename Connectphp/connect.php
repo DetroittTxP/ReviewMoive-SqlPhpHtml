9f3bc03e8204f31c3495f7cb79e7fb9158fdce4b
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2022-10-08 23:05:34 +0700

    login but fail

--- Connectphp/connect.php
+++ Connectphp/connect.php
@@ -1,6 +1,6 @@
 <?php
-    $gg = new PDO("mysql:host=localhost;dbname=moviesratings;charset=utf8","root","");
-    $gg->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=localhost;dbname=moviesratings;charset=utf8", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 ?>
--- register/RegisterMove.php
+++ register/RegisterMove.php
@@ -5,9 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RegisterWebpage</title>
+    <title>RegisterWebPage</title>
+
+    <?php  
+        $abc=$pdo->prepare("INSERT INTO member VALUES('',?,?,?)");
+        $abc->bindParam(1,$_POST["username"]);
+        $abc->bindParam(2,$_POST["password"]);
+        $abc->bindParam(3,$_POST["email"]);
+
+        $abc->execute();
+
+    ?>
 </head>
 <body>
-    
+    เพิ่มสมาชิกสำเร็จเเล้ว!!!!!
+    <br>
+    <br>
+    <a href="../ReviewMovie/index.html">กลับไปหน้าหลัก</a>
 </body>
 </html>
