a20579f70178f040cee9194631e03767362b17a5
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2022-10-09 19:53:58 +0700

    PDO

--- Connectphp/connect.php
+++ Connectphp/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo = new PDO("mysql:host=localhost;dbname=moviesratings;charset=utf8", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=movierating;charset=utf8", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
