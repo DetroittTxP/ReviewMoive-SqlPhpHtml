98cf854702a2ea0dde3748b5b30810deb845bf3c
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2022-10-29 19:18:12 +0700

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="log.css">
+</head>
+<body>
+    <header>
+        <img src="../img/LogoWeb.png" alt="" height="215px">
+    </header>
+    <fieldset>
+        <legend>Login</legend><br>
+        <label>Username: </label>
+        <input type="text" name="username" placeholder="Username"><br><br>
+        <label>Password: </label>
+        <input type="password" name="password"  placeholder="Password"><br><br>
+        <label>Please type the required numbers: 4786 </label><br>
+        <input type="text" placeholder="Please Enter Number"><br>
+        <br>
+        <form action="/register/register.html">
+            <input type="submit" value="Register">
+        </form>
+        <form action="/ReviewMovie/index.php">
+            <input type="submit" value="Login">
+        </form>
+    </fieldset>
+</body>
+</html>
