41e376216df29c98ff57c41d49b37929e9e23fb9
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2022-11-22 14:17:24 +0700

    เเก้ไข หน้า search

--- SearchPage/process.php
+++ SearchPage/process.php
@@ -10,19 +10,22 @@
         echo mysql_errno();
     }
 
-    $sql = "SELECT * FROM movie WHERE movie_name LIKE '%$keyword%' ";
+    $sql = "SELECT movie.movie_name,movie.movie_type, ROUND(AVG(movie_score),2) 
+    FROM movie JOIN comment 
+    WHERE movie_name LIKE '%mi%';
+    ";
     $objQuery = mysqli_query($conn,$sql);
 ?>
 
 <?php while($row = mysqli_fetch_array($objQuery)){?>
          <div class="img">
-            <img src="../img/ALLIMGS/<?=$row["movie_id"]?>.jpg" height="350px">
+            <img src="../img/ALLIMGS/<?=$row["movie_name"]?>.jpg" height="350px">
         </div>
   
         <div class="information">
             ชื่อเรื่อง: <?=$row["movie_name"]?><br>
             ประเภท: <?=$row["movie_type"]?><br>
-            คะเเนน: <?=$row["movie_score"]?><br>
+            คะเเนน: <?=$row["ROUND(AVG(movie_score),2)"]?><br>
             <a href="#" onclick="return plslogin1()">
                 <img src="img/review-button-png-hi.png" width = "70px">
             </a>
